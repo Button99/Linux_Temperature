542d3522c19ca2056344a8e178f7dd8f240ccd4a
Button99 <56029580+Button99@users.noreply.github.com>
2020-05-08 11:58:45 +0300

    Script

---
+++ Temp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "Your PC temperature is"
+while [ true ]
+do
+clear
+date
+
+echo "CPU Temperature: "
+
+cat "/sys/class/hwmon/hwmon3/temp1_input"
+
+echo "Ambient Temperature: "
+
+cat "/sys/class/hwmon/hwmon3/temp2_input"
+
+echo "SODDIM Temperature: "
+
+cat "/sys/class/hwmon/hwmon3/temp3_input"
+
+if ! read -sn 1 -t 1 cmd ; then
+continue
+fi
+
+if [ $cmd == 'q' ] ; then
+break
+fi
+
+done
+exit
